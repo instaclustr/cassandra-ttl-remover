f4643cfb04551e1e2a20972d1cc105ab6ce36418
Alexander Ivakov <alex.ivakov@instaclustr.com>
2018-08-01 15:45:03 +1000

    Big refactor to make TTLRemover work with SSTables from Cassandra 3.0

--- src/main/java/org/apache/cassandra/noTTL/TTLRemover.java
+++ src/main/java/org/apache/cassandra/noTTL/TTLRemover.java
@@ -17,65 +17,110 @@
  */
 package org.apache.cassandra.noTTL;
 
-import org.apache.cassandra.config.Config;
 import org.apache.cassandra.config.CFMetaData;
 import org.apache.cassandra.config.Schema;
 import org.apache.cassandra.db.*;
+import org.apache.cassandra.db.compaction.OperationType;
+import org.apache.cassandra.db.lifecycle.LifecycleTransaction;
+import org.apache.cassandra.db.partitions.PartitionUpdate;
+import org.apache.cassandra.db.rows.*;
 import org.apache.cassandra.exceptions.ConfigurationException;
 import org.apache.cassandra.io.sstable.Descriptor;
 import org.apache.cassandra.io.sstable.ISSTableScanner;
 import org.apache.cassandra.io.sstable.KeyIterator;
+import org.apache.cassandra.io.sstable.SSTableRewriter;
+import org.apache.cassandra.io.sstable.format.SSTableFormat;
+import org.apache.cassandra.io.sstable.format.SSTableReader;
 import org.apache.cassandra.io.sstable.format.SSTableWriter;
-import org.apache.cassandra.service.ActiveRepairService;
-import org.apache.cassandra.utils.JVMStabilityInspector;
 import org.apache.cassandra.tools.Util;
+import org.apache.cassandra.utils.FBUtilities;
+import org.apache.cassandra.utils.JVMStabilityInspector;
 import org.apache.commons.cli.*;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.util.Iterator;
+import java.util.ArrayList;
 
 /**
  * Do batch TTL removing on table
  */
 public class TTLRemover {
     private static CommandLine cmd;
-    private static Options options = new Options();;
+    private static Options options = new Options();
     private static final String OUTPUT_PATH = "p";
 
     static
     {
         Option outputPath = new Option(OUTPUT_PATH, true, "Output directory");
         options.addOption(outputPath);
+        Util.initDatabaseDescriptor();
+        Schema.instance.loadFromDisk(false);
     }
 
 
     private static void stream(Descriptor descriptor, Descriptor toSSTable) throws IOException {
-        long keyCount = countKeys(descriptor);
 
-        NoTTLReader noTTLreader = NoTTLReader.open(descriptor);
+        SSTableReader noTTLreader = SSTableReader.open(descriptor);
 
         ISSTableScanner noTTLscanner = noTTLreader.getScanner();
 
-        ColumnFamily columnFamily = ArrayBackedSortedColumns.factory.create(descriptor.ksname, descriptor.cfname);
+        ColumnFamilyStore columnFamily = ColumnFamilyStore.getIfExists(descriptor.ksname, descriptor.cfname);
 
-        SSTableWriter writer = SSTableWriter.create(toSSTable, keyCount, ActiveRepairService.UNREPAIRED_SSTABLE);
+        long keyCount = countKeys(descriptor, columnFamily.metadata);
 
-        NoTTLSSTableIdentityIterator row;
+        LifecycleTransaction txn = LifecycleTransaction.offline(OperationType.WRITE);
+
+        SerializationHeader header = SerializationHeader.makeWithoutStats(columnFamily.metadata);
+
+        SSTableRewriter writer = SSTableRewriter.constructKeepingOriginals(txn, true, Long.MAX_VALUE, true);
+        writer.switchWriter(SSTableWriter.create(toSSTable.toString(), keyCount, -1, header, txn));
+        UnfilteredRowIterator partition;
 
         try
         {
-            while (noTTLscanner.hasNext()) //read data from disk //NoTTLBigTableScanner
+            while (noTTLscanner.hasNext()) //read data from disk
             {
-                row = (NoTTLSSTableIdentityIterator) noTTLscanner.next();
-                serializeRow(row,columnFamily, row.getColumnFamily().metadata());
-                writer.append(row.getKey(), columnFamily);
-                columnFamily.clear();
+                partition = noTTLscanner.next();
+
+                if(!partition.hasNext()){
+                    //keep partitions with no rows
+                    writer.append(partition);
+                    continue;
+                }
+                PartitionUpdate u = new PartitionUpdate(columnFamily.metadata, partition.partitionKey(), partition.columns(), 2);
+                RangeTombstone.Bound start = null;
+                RangeTombstone.Bound end = null;
+
+                while(partition.hasNext()) {
+                    Unfiltered unfiltered = partition.next();
+                    switch(unfiltered.kind()){
+                        case ROW:
+                            Row newRow = serializeRow(unfiltered, columnFamily.metadata);
+                            u.add(newRow);
+                            break;
+                        case RANGE_TOMBSTONE_MARKER:
+                            //Range tombstones are denoted as separate (Unfiltered) entries for start and end,
+                            //so we record them separately and add the tombstone once both ends of the range are defined
+                            RangeTombstoneBoundMarker marker = (RangeTombstoneBoundMarker) unfiltered;
+
+                            if(marker.isOpen(false)){
+                                start = marker.openBound(false);
+                            }
+                            if(marker.isClose(false)){
+                                end = marker.closeBound(false);
+                            }
+                            if(start != null && end != null){
+                                u.add(new RangeTombstone(Slice.make(start, end), marker.deletionTime()));
+                                start = null;
+                                end = null;
+                            }
+                            break;
+                    }
+                }
+
+                writer.append(u.unfilteredIterator());
             }
-
-            writer.finish(true);
-
+            writer.finish();
         }
         finally
         {
@@ -84,35 +129,28 @@ public class TTLRemover {
 
     }
 
-    private static void serializeRow(Iterator<OnDiskAtom> atoms, ColumnFamily columnFamily, CFMetaData metadata) {
+    private static Row serializeRow(Unfiltered atoms, CFMetaData metaData) {
 
-        while (atoms.hasNext())
+        Row row = (Row) atoms;
+
+        ArrayList<Cell> celllist = new ArrayList<>();
+
+        for(Cell cell: row.cells())
         {
-            serializeAtom(atoms.next(), metadata, columnFamily);
+            if (cell.isExpiring())
+            {
+                celllist.add(BufferCell.live(metaData, cell.column(), cell.timestamp(), cell.value()));
+            }
+            else {
+                celllist.add(cell);
+            }
         }
 
+        return BTreeRow.create(row.clustering(), LivenessInfo.create(metaData, row.primaryKeyLivenessInfo().timestamp(), FBUtilities.nowInSeconds()), row.deletion(), celllist.toArray());
     }
 
-    private static void serializeAtom(OnDiskAtom atom, CFMetaData metadata, ColumnFamily columnFamily) {
-        if (atom instanceof Cell)
-        {
-            Cell cell = (Cell) atom;
-            if (cell instanceof BufferExpiringCell)
-            {
-                columnFamily.addColumn(cell.name(),cell.value(),cell.timestamp());
-            }
-            else if (cell instanceof BufferDeletedCell)
-            {
-
-            }
-            else
-                columnFamily.addColumn(cell);
-
-        }
-    }
-
-    private static long countKeys(Descriptor descriptor) {
-        KeyIterator iter = new KeyIterator(descriptor);
+    private static long countKeys(Descriptor descriptor, CFMetaData metaData) {
+        KeyIterator iter = new KeyIterator(descriptor, metaData);
         long keycount = 0;
         try
         {
@@ -127,7 +165,6 @@ public class TTLRemover {
         {
             iter.close();
         }
-        System.out.println("Key count: "+keycount);
         return keycount;
     }
 
@@ -154,12 +191,6 @@ public class TTLRemover {
 
         String fromSSTable = new File(cmd.getArgs()[0]).getAbsolutePath();
 
-        Util.initDatabaseDescriptor();
-        Config.setClientMode(true);
-
-        Schema.instance.loadFromDisk(false);  //load kspace "systemcf" and its tables;
-        Keyspace.setInitialized();
-
         Descriptor descriptor = Descriptor.fromFilename(fromSSTable);
 
         if (Schema.instance.getKSMetaData(descriptor.ksname) == null)
@@ -175,8 +206,7 @@ public class TTLRemover {
                 String outputFolder = cmd.getOptionValue(OUTPUT_PATH);
                 String toSSTableDir = outputFolder + File.separator + descriptor.ksname + File.separator + descriptor.cfname;
                 File directory = new File(toSSTableDir);
-                directory.mkdirs();
-                Descriptor resultDesc = new Descriptor(directory, descriptor.ksname, descriptor.cfname, descriptor.generation, Descriptor.Type.FINAL);
+                Descriptor resultDesc = new Descriptor(directory, descriptor.ksname, descriptor.cfname, descriptor.generation, SSTableFormat.Type.BIG);
                 stream(descriptor, resultDesc);
             }
             else
